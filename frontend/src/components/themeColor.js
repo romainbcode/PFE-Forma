ed37832a3444beb158670a40010aaee420a309c5
romain bizot <romain-bizot@hotmail.fr>
2023-11-02 21:31:49 +0100

    Ajout du theme color et mise en place dans le fichier index

---
+++ frontend/src/components/themeColor.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material';
+  
+export const theme = createTheme({
+
+    palette: {
+        primary: {
+            main: '#fffffe',
+            background: '#0f0e17',
+            headLine: '#fffffe',
+            paragraph: '#a7a9be',
+            button_background: '#ff8906',
+            button_text: '#fffffe',
+            stroke: '#000',
+            secondary: '#f25f4c',
+            tertiary: "#e53170"
+        },
+    }
+})
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Auth0ProviderWithHistory from './auth0-provider-with-navigate';
+import { ThemeProvider } from '@mui/material'
+import { theme } from './components/themeColor.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <Auth0ProviderWithHistory>
-        <App />
-    </Auth0ProviderWithHistory>
+      <Auth0ProviderWithHistory>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Auth0ProviderWithHistory>
     </Router>
   </React.StrictMode>
 );
