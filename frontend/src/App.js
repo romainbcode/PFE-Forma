0dab4c36466fc96bd36fa3eccfe63d40d0251611
romain bizot <romain-bizot@hotmail.fr>
2023-11-06 14:26:09 +0100

    ajout de la route par défaut pour l'erreur 404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/homepage";
+import { Profile } from "./pages/profile";
+import { Erreur404 } from "./pages/erreur-404";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<Erreur404 />} />
     </Routes>
   );
-}
+};
 
 export default App;
