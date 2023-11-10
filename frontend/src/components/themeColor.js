351b49d832553a3756eed1a184a8cd135bdc2a23
romain bizot <romain-bizot@hotmail.fr>
2023-11-10 12:40:53 +0100

    modification de la couleur du background : #252525

--- frontend/src/components/themeColor.js
+++ frontend/src/components/themeColor.js
@@ -1,18 +1,17 @@
-import { createTheme } from '@mui/material';
-  
-export const theme = createTheme({
+import { createTheme } from "@mui/material";
 
-    palette: {
-        primary: {
-            main: '#fffffe',
-            background: '#0f0e17',
-            headLine: '#fffffe',
-            paragraph: '#a7a9be',
-            button_background: '#ff8906',
-            button_text: '#fffffe',
-            stroke: '#000',
-            secondary: '#f25f4c',
-            tertiary: "#e53170"
-        },
-    }
-})
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fffffe",
+      background: "#252525",
+      headLine: "#fffffe",
+      paragraph: "#a7a9be",
+      button_background: "#ff8906",
+      button_text: "#fffffe",
+      stroke: "#000",
+      secondary: "#f25f4c",
+      tertiary: "#e53170",
+    },
+  },
+});
