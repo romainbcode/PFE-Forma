b62d14e7bfa6fd7f2dc0f11756adf11e15e45be5
romain bizot <romain-bizot@hotmail.fr>
2023-11-13 22:09:12 +0100

    ajout de la route pour la page qui affiche toutes les formations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/homepage";
-import { Profile } from "./pages/profile";
 import { Erreur404 } from "./pages/erreur-404";
+import { ChoixFormation } from "./pages/choix-formation";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/formations" element={<ChoixFormation />} />
       <Route path="*" element={<Erreur404 />} />
     </Routes>
   );
