e1f3331cffbd5a851210502fb56362c278f4f41e
romain bizot <romain-bizot@hotmail.fr>
2023-11-13 22:10:19 +0100

    Navbar dans ce fichier pour mettre la NavBar dans toutes les pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Auth0ProviderWithHistory from './auth0-provider-with-navigate';
-import { ThemeProvider } from '@mui/material'
-import { theme } from './components/themeColor.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth0-provider-with-navigate";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./components/themeColor.js";
+import { Navbar } from "./components/navbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <Auth0ProviderWithHistory>
         <ThemeProvider theme={theme}>
+          <Navbar />
           <App />
         </ThemeProvider>
       </Auth0ProviderWithHistory>
