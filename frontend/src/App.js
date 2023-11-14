342ae312369faa2047de67fd7a5bd8ee64641a12
romain bizot <romain-bizot@hotmail.fr>
2023-11-14 11:20:44 +0100

    ajout de la route pour la creation de formations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/homepage";
 import { Erreur404 } from "./pages/erreur-404";
 import { ChoixFormation } from "./pages/choix-formation";
+import { CreationFormation } from "./pages/professeur/creation-formation";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/formations" element={<ChoixFormation />} />
+      <Route path="/formations/create" element={<CreationFormation />} />
       <Route path="*" element={<Erreur404 />} />
     </Routes>
   );
