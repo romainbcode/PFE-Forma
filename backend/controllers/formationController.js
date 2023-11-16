0aa2cdf59dd3822e8acdf93951f932cadd2f24ef
romain bizot <romain-bizot@hotmail.fr>
2023-11-16 14:42:52 +0100

    Récupére les informations provenant du frontend et création de formation fonctionne

--- backend/controllers/formationController.js
+++ backend/controllers/formationController.js
@@ -1,43 +1,15 @@
 const Formation = require("../models/formationModel");
 
 exports.createFormation = async (req, res, next) => {
-  const titleforma = "Titre forma";
-  const descforma = "Description forma";
-  const titlechapitre = "Titre chapitre";
-  const descchapitre = "Description chapitre";
-  const titlesouschapitre = "Titre sous-chapitre";
-  const descsouschapitre = "Description sous-chapitre";
-  const text = "corps-text";
-  const textattention = "corps-texte-attention";
-  const textconseil = "corps-texte-conseil";
+  const { titre } = req.body;
+  const { description } = req.body;
+  const { chapitre } = req.body;
+
   try {
     const formation = await Formation.create({
-      title: titleforma,
-      description: descforma,
-      chapitre: [
-        {
-          titre_chapitre: titlechapitre,
-          description_chapitre: descchapitre,
-          sous_chapitre: [
-            {
-              titre_sous_chapitre: titlesouschapitre,
-              description_sous_chapitre: descsouschapitre,
-              corps_texte_image: [
-                {
-                  texte: text,
-                  texte_attention: textattention,
-                  texte_conseil: textconseil,
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      note: [
-        {
-          note: 12,
-        },
-      ],
+      titre: titre,
+      description: description,
+      chapitre: chapitre,
     });
 
     res.status(201).json({
