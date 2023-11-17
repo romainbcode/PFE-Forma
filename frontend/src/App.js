a70ec420efab4ae3fc57d5a58f7a4f29dd1a71be
romain bizot <romain-bizot@hotmail.fr>
2023-11-17 16:16:38 +0100

    Ajout de la route pour aller sur la page de la création de quiz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,14 @@ import { ChoixFormation } from "./pages/choix-formation";
 import { CreationFormation } from "./pages/professeur/creation-formation";
 import { FormationAccueil } from "./pages/formationAccueil";
 import { Formation } from "./pages/formation";
+import { CreationQuiz } from "./pages/professeur/creation-quiz";
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/formations" element={<ChoixFormation />} />
       <Route path="/formations/create" element={<CreationFormation />} />
+      <Route path="/quizs/create" element={<CreationQuiz />} />
       <Route path="/formation/:formation_id" element={<FormationAccueil />} />
       <Route
         path="/formation/:formation_id/:chapitre_id"
