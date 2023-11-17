791076067694ca490664db97713c9d659a57acd3
romain bizot <romain-bizot@hotmail.fr>
2023-11-17 09:50:51 +0100

    Pages affichant un résumé de la formation et un bouton en bas pour commencer celle ci

---
+++ frontend/src/pages/formationAccueil.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState, useCallback, useMemo } from "react";
+import { Box, Typography, Button, Link } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { Loader } from "../components/loader/loader";
+import { ChapitreDescriptionAccueil } from "../components/chapitre-description-accueil";
+import { format } from "date-fns";
+
+import axios from "axios";
+
+export const FormationAccueil = (props) => {
+  const [formationById, setFormationById] = useState([]);
+  const [chapitresFormation, setChapitresFormation] = useState([]);
+  const [dateCreation, setDateCreation] = useState();
+  const [isloading, setIsLoading] = useState(true);
+
+  const { formation_id } = useParams();
+  const getFormationById = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:3000/formation/" + formation_id
+      );
+      setFormationById(data.formationById);
+      const date = new Date(formationById.createdAt);
+      const dateformatted = format(date, "dd/MM/yyyy");
+      setDateCreation(dateformatted);
+      setChapitresFormation(formationById.chapitre);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  const memoizedFormationById = useMemo(() => getFormationById, []);
+
+  useEffect(() => {
+    setFormationById(memoizedFormationById);
+  }, [memoizedFormationById]);
+
+  return (
+    <Box>
+      {formationById.titre + " date de création : "}
+      {dateCreation}
+      {isloading ? (
+        <Loader />
+      ) : (
+        chapitresFormation &&
+        chapitresFormation.map((chapitreFormation, index) => (
+          <Box key={index}>
+            <ChapitreDescriptionAccueil
+              titre={chapitreFormation.titre_chapitre}
+              description={chapitreFormation.description_chapitre}
+            />
+          </Box>
+        ))
+      )}
+      <Link to={`/formation/${formationById._id}`}>OK</Link>
+    </Box>
+  );
+};
+
+export default FormationAccueil;
