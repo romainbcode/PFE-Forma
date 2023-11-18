4d2496265fefe50edfc05c644ac91a8681e638b5
romain bizot <romain-bizot@hotmail.fr>
2023-11-18 20:19:13 +0100

    Ajout d'une méthode qui ajoute l'utilisateur dans la BDD si il n'y est pas déjà

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,57 @@
 const User = require("../models/userModel");
 
 exports.createUser = async (req, res, next) => {
-  const iduserauth = "1234567890123";
+  const { id_user_auth } = req.body;
   try {
-    const user = await User.create({
-      id_user_auth: iduserauth,
-      scores: [
-        {
-          //id_quiz: ,
-          scores_pourcentage: 80,
-        },
-      ],
-      niveau: 1,
+    const userExistant = await User.findOne({
+      id_user_auth: id_user_auth,
     });
+    if (!userExistant) {
+      const user = await User.create({
+        id_user_auth: id_user_auth,
+        niveau: 1,
+      });
 
-    res.status(201).json({
-      success: true,
-      user,
-    });
+      res.status(201).json({
+        success: true,
+        user,
+        message: "Utilisateur ajouté à la BDD.",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        userExistant,
+        message: "Utilisateur déjà enregistré dans la BDD.",
+      });
+    }
   } catch (error) {
     res.status(400).json({
+      success: false,
+    });
+  }
+};
+
+exports.getUserByIdUserAuth = async (req, res, next) => {
+  const { iduserauth } = req.params;
+  try {
+    const userExistant = await User.findOne({
+      id_user_auth: iduserauth,
+    });
+    console.log(userExistant);
+    if (userExistant) {
+      res.status(200).json({
+        success: true,
+        userExistant,
+        message: "Utilisateur déjà enregistré dans la BDD.",
+      });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: "Utilisateur non enregistré dans la BDD.",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
       success: false,
     });
   }
