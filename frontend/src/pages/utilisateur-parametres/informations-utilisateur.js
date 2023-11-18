d3fa77dac789a8c72a0a5069a8261563bf1cb14f
romain bizot <romain-bizot@hotmail.fr>
2023-11-18 20:48:51 +0100

    Ajout des 3 pages sur les informations de l'utilisateur

---
+++ frontend/src/pages/utilisateur-parametres/informations-utilisateur.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { ListeMenuInfosUtilisateur } from "../../components/listeMenuInfosUtilisateur";
+
+export const InformationsUtilisateur = () => {
+  return (
+    <Box>
+      <ListeMenuInfosUtilisateur />
+      infos
+    </Box>
+  );
+};
+
+export default InformationsUtilisateur;
---
+++ frontend/src/pages/utilisateur-parametres/scores-utilisateur.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { ListeMenuInfosUtilisateur } from "../../components/listeMenuInfosUtilisateur";
+
+export const ScoresUtilisateur = () => {
+  return (
+    <Box>
+      <ListeMenuInfosUtilisateur />
+      scores
+    </Box>
+  );
+};
+
+export default ScoresUtilisateur;
