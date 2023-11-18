cb0b9f3b4c5d3b17d88e969534b5cf59a997832c
romain bizot <romain-bizot@hotmail.fr>
2023-11-18 20:18:28 +0100

    Factorisation du code

--- frontend/src/components/login-button.js
+++ frontend/src/components/login-button.js
@@ -1,9 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 import React from "react";
 
 export const LoginButton = () => {
-  
   const { loginWithRedirect } = useAuth0();
 
   const handleLogin = async () => {
@@ -11,16 +10,19 @@ export const LoginButton = () => {
   };
 
   return (
-      <Button className="button__login" onClick={handleLogin} 
-        sx={{bgcolor: "primary.button_background",
-        borderRadius: '5px',
-        padding: '5px 20px',
-        transition: 'background-color 0.3s',
-        fontWeight:'bold',
-        }}
-      >
-        Log In
-      </Button>
+    <Button
+      className="button__login"
+      onClick={handleLogin}
+      sx={{
+        bgcolor: "primary.button_background",
+        borderRadius: "5px",
+        padding: "5px 20px",
+        transition: "background-color 0.3s",
+        fontWeight: "bold",
+      }}
+    >
+      Log In
+    </Button>
   );
 };
 
