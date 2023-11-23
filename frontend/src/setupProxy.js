ef67298e53309818c958040615567dc2f18d43d1
romain bizot <romain-bizot@hotmail.fr>
2023-11-23 14:47:57 +0100

    Creation d'un fichier pour pouvoir definir plusieurs proxy

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,20 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api-node",
+    createProxyMiddleware({
+      target: "http://localhost:3000",
+      changeOrigin: true,
+      pathRewrite: { "^/api-node": "" },
+    })
+  );
+  app.use(
+    "/api-python",
+    createProxyMiddleware({
+      target: "http://localhost:3002",
+      changeOrigin: true,
+      pathRewrite: { "^/api-python": "" },
+    })
+  );
+};
