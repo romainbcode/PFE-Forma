aec0d28ab9a9538beaa872d7dbdac61b11ce5ba7
romain bizot <romain-bizot@hotmail.fr>
2023-11-23 14:50:24 +0100

    Capable d'envoyer des mails (faut juste decommenter le code)

--- backend/python/mail.py
+++ backend/python/mail.py
@@ -1,21 +1,61 @@
 import smtplib
 import os
+from flask import *
+from flask_cors import CORS
 from dotenv import load_dotenv
+
+
+
+app = Flask(__name__)
+CORS(app)
+
 
 load_dotenv()
 
-server = 'smtp.gmail.com'
-#Port SMTP : choix de 587 car prend en charge TLS
-port = 587 
-username = os.getenv('USERNAME')
-password = os.getenv('PASSWORD')
-#Création instance client SMTP
-client = smtplib.SMTP(server, port)
-#Activation protocole de sécurité TLS
-client.starttls()
-#Authentification sur la serveur
-client.login(username, password)
-#Envoie mail
-client.sendmail(username, "romain_bizot@etu.u-bourgogne.fr", "TEST")
-#Fermeture de la connexion
-client.quit()
+@app.route('/mail', methods=['POST'])
+def envoieMail():        
+    data = request.get_json()
+
+    nomUser = data["nomUser"]
+    emailUser = data["emailUser"]
+    nomFormation = data["nomFormation"]
+    
+    corpsMail = "Cher " + str(nomUser) + "\n" \
+        "📝 Nous sommes ravis de vous accueillir dans notre formation " + str(nomFormation) + "! \n" \
+        "🚀 Felicitations pour ce premier pas vers l'apprentissage et l'acquisition de nouvelles compétences. Nous sommes convaincus que cette expérience sera enrichissante et source d'inspiration pour vous.\n"\
+        "Nos formateurs dévoués sont la pour vous accompagner à chaque étape. N'hesitez pas à poser des questions à notre robot 🤖!\n"\
+        "Nous sommes impatients de partager ce voyage avec vous et de celebrer vos reussites tout au long de la formation."\
+        "Bonne chance ! Nous sommes convaincus que vous brillerez comme jamais dans cette nouvelle etape de votre parcours."\
+        "Cordialement,"\
+        "L'équipe de [Votre Entreprise]"
+    
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    """
+    server = 'smtp.gmail.com'
+
+    #Port SMTP : choix de 587 car prend en charge TLS
+    port = 587 
+
+    #Création instance client SMTP
+    client = smtplib.SMTP(server, port)
+
+    #Activation protocole de sécurité TLS
+    client.starttls()
+
+    #Authentification sur la serveur
+    client.login(username, password)
+
+    #Envoie mail
+    client.sendmail(username, "romain_bizot@etu.u-bourgogne.fr", msg=f"{corpsMail}".encode('utf-8'),)
+
+    #Fermeture de la connexion
+    client.quit()
+    """ 
+    
+
+    return "mail envoyé"
+    
+if __name__ == '__main__':
+    app.run(host='localhost', port=3002, debug=True)
+    
