44a43d2d75158d642b9d070a45fa9fc4abcf9a30
romain bizot <romain-bizot@hotmail.fr>
2023-11-24 18:10:20 +0100

    getQuizById retourne le quiz en fonction de son id

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -1,13 +1,33 @@
 const Quiz = require("../models/quizModel");
 
 exports.createQuiz = async (req, res, next) => {
-  const { question_reponse } = req.body;
+  const { question_reponse, titre, id_user_auth } = req.body;
   try {
     const quiz = await Quiz.create({
+      titre: titre,
+      createdBy: id_user_auth,
       question_reponse: question_reponse,
     });
 
-    res.status(201).json({
+    res.status(200).json({
+      success: true,
+      quiz,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+exports.getQuizById = async (req, res, next) => {
+  const { id_quiz } = req.body;
+  console.log(id_quiz);
+  try {
+    const quiz = await Quiz.findById(id_quiz);
+
+    res.status(200).json({
       success: true,
       quiz,
     });
