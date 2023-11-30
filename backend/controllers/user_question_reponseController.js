73aef0b2676143ce63ef1d8ccc4dbff141ee982e
romain bizot <romain-bizot@hotmail.fr>
2023-11-30 14:18:38 +0100

    Ajoute le choix d'un user dans un quiz a la BDD

--- backend/controllers/user_question_reponseController.js
+++ backend/controllers/user_question_reponseController.js
@@ -1,12 +1,31 @@
 const UserQuestionReponse = require("../models/user_question_reponseModel");
+const Quiz = require("../models/quizModel");
 
 exports.createUserQuestionReponse = async (req, res, next) => {
+  const { id_user_auth, quiz_id, reponses } = req.body;
   try {
-    const user_question_reponse = await UserQuestionReponse.create({});
+    const userQuestionReponses = Object.keys(reponses).map((key) => {
+      const item = reponses[key];
+      return {
+        id_question: item.question_id,
+        id_reponse: item.reponse_id,
+        etat_reponse_user: true,
+      };
+    });
+
+    const newUser_question_reponse = await UserQuestionReponse.create({
+      id_user: id_user_auth,
+      user_question_reponse: [
+        {
+          id_quiz: quiz_id,
+          question_reponse: userQuestionReponses,
+        },
+      ],
+    });
 
     res.status(201).json({
       success: true,
-      user_question_reponse,
+      newUser_question_reponse,
     });
   } catch (error) {
     res.status(400).json({
