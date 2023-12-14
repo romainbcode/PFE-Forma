b1c5eb90d2661ecc826071537a2d50ceb5060715
romain bizot <romain-bizot@hotmail.fr>
2023-12-14 10:57:50 +0100

    Ajout du numéro du chapitre

--- frontend/src/components/listeChapitreFormation.js
+++ frontend/src/components/listeChapitreFormation.js
@@ -2,7 +2,14 @@ import React from "react";
 import { Box } from "@mui/material";
 
 export const ListeChapitreFormation = (props) => {
-  return <Box>{props.chapitre.titre_chapitre}</Box>;
+  return (
+    <Box>
+      {"Chapitre " +
+        props.numeroChapitre +
+        " : " +
+        props.chapitre.titre_chapitre}
+    </Box>
+  );
 };
 
 export default ListeChapitreFormation;
