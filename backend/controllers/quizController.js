fb56ef44dbe79189ec5fd77e8119d962466977f0
romain bizot <romain-bizot@hotmail.fr>
2023-12-14 15:00:59 +0100

    Route + Méthode qui retourne la liste des bonnes réponses pour un quiz

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -23,13 +23,41 @@ exports.createQuiz = async (req, res, next) => {
 
 exports.getQuizById = async (req, res, next) => {
   const { id_quiz } = req.body;
-  console.log(id_quiz);
   try {
     const quiz = await Quiz.findById(id_quiz);
 
     res.status(200).json({
       success: true,
       quiz,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+exports.getAllTrueReponseQuizById = async (req, res, next) => {
+  const { id_quiz } = req.body;
+  try {
+    const quiz = await Quiz.findById(id_quiz);
+    if (!quiz) {
+      return res.status(404).json({
+        success: false,
+        message: "Quiz non trouvé",
+      });
+    }
+
+    //map : renvoit : Array(2) : 0:["ijij"], 1:["okoko"] : sur plusieurs tableaux
+    //FlatMap renvoit : Array(2) : 0:"ijij", 1:"okoko" : applait le résultat en un seul tableau
+    const trueReponses = quiz.question_reponse.flatMap((qr) =>
+      qr.reponse.filter((rep) => rep.etat_reponse).map((rep) => rep._id)
+    );
+
+    res.status(200).json({
+      success: true,
+      trueReponses,
     });
   } catch (error) {
     res.status(400).json({
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { getAllTrueReponseQuizById } = require("../controllers/quizController");
+const { isUser } = require("../middleware/auth");
+
+router.post("/getTrueReponse", isUser, getAllTrueReponseQuizById);
 
 module.exports = router;
