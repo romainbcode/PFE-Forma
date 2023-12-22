b69ef15e9660bd45b64a11e7efd4657a375252c8
romain bizot <romain-bizot@hotmail.fr>
2023-12-22 19:44:29 +0100

    Loader pour quand le bot écris

---
+++ frontend/src/chat-bot/loader-ecriture/loader-ecriture.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./loader-ecriture.css";
+export const LoaderEcriture = () => {
+  return (
+    <section class="dots-container">
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </section>
+  );
+};
+
+export default LoaderEcriture;
