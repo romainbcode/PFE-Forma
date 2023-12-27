e6fbdcf56fd7fa6d419ca344374cc930486a7368
romain bizot <romain-bizot@hotmail.fr>
2023-12-27 17:41:23 +0100

    Ajout axios dans la card des cours et modif des couleurs

--- frontend/src/components/courscard/coursCard.js
+++ frontend/src/components/courscard/coursCard.js
@@ -1,18 +1,57 @@
 import React from "react";
 import "./coursCard.css";
-import { Box, Typography, Link, Button } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Toaster, toast } from "sonner";
+
+const url_back_node = process.env.REACT_APP_BACKNODE;
 
 export const CoursCard = (props) => {
+  const { getAccessTokenSilently, user } = useAuth0();
+
+  const test = async () => {
+    const token = await getAccessTokenSilently();
+    const config = {
+      headers: {
+        Authorization: `${token}`,
+      },
+    };
+    const data = await axios.post(
+      url_back_node + "/cours/userSubscription",
+      {
+        id_user_auth: props.id_prof,
+        id_cours: props.id,
+        id_user_auth_subscription: user.sub,
+      },
+      config
+    );
+    if (data.status === 200) {
+      toast.error(data.data.message);
+    } else if (data.status === 201) {
+      toast.success(data.data.message);
+    }
+  };
   return (
-    <Box class="card">
-      <Box class="card-border-top"></Box>
-      <Box class="img"></Box>
-      <Box sx={{ textAlign: "center" }}>
-        <Typography> Person</Typography>
+    <>
+      <Toaster expand={true} richColors />
+      <Box class="card">
+        <Box class="card-border-top"></Box>
+        <Box>{props.titre}</Box>
+        <Box class="img"></Box>
+        <Box sx={{ textAlign: "center" }}>
+          <Typography> </Typography>
+        </Box>
+        <Typography class="job">{props.description} </Typography>
+        <Button
+          onClick={() => {
+            test();
+          }}
+        >
+          S'inscrire
+        </Button>
       </Box>
-      <Typography class="job"> {props.titre}</Typography>
-      <Button> S'inscrire</Button>
-    </Box>
+    </>
   );
 };
 
